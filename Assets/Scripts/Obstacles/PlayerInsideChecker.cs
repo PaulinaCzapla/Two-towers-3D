d1c1f1dbe8187233260c65b2cd45d093458d5df8
Paulina Czapla <paulina.czp00@gmail.com>
2022-06-21 01:37:34 +0200

    moving platform

---
+++ Assets/Scripts/Obstacles/PlayerInsideChecker.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using UnityEngine;
+
+namespace Obstacles
+{
+    [Serializable]
+    public class PlayerInsideChecker
+    {
+        [Header("Player detection")]
+        [SerializeField] private LayerMask playerLayer;
+        [SerializeField] private Vector3 checkPosCorrection = Vector3.zero;
+        [SerializeField] private Vector3 halfExtends;
+        [SerializeField] private Transform objTransform;
+
+        public GameObject CheckIfPlayerInside()
+        {
+            Collider[] hitColliders = Physics.OverlapBox(objTransform.transform.position, halfExtends,
+                Quaternion.identity, playerLayer);
+
+            if (hitColliders.Length > 0)
+            {
+                return hitColliders[0].gameObject;
+            }
+            return null;
+        }
+
+        public void OnDrawGizmos()
+        {
+            Gizmos.DrawWireCube(checkPosCorrection+ objTransform.position, halfExtends);
+        }
+
+    }
+}
