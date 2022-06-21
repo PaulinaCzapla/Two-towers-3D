77edd7dbf71bca09a4066fd3fae7d9c264f82424
Paulina Czapla <paulina.czp00@gmail.com>
2022-06-21 18:09:36 +0200

    Platforms

---
+++ Assets/Scripts/Obstacles/Platforms/FallingPlatform.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using Input;
+using Timers;
+using UnityEngine;
+
+namespace Obstacles.Platforms
+{
+    public class FallingPlatform : MonoBehaviour
+    {
+        [Header("Platform params")]
+        [SerializeField] private float timeBeforeFall = 1;
+        [SerializeField] private float timeBeforeRespawn = 3;
+        
+        [Header("Components")]
+        [SerializeField] private PlayerInsideChecker checker;
+        [SerializeField] private Rigidbody rb;
+        [SerializeField] private Collider platformCollider;
+        [SerializeField] private GameObject mesh;
+
+        private Cooldown _fallCooldown;
+        private Cooldown _shakeCooldown;
+        private Cooldown _restoreCooldown;
+        private Vector2 _initialPosition;
+
+        private void Awake()
+        {
+            _initialPosition = transform.position;
+        }
+
+        private void FixedUpdate()
+        {
+            if (_shakeCooldown != null && _shakeCooldown.CooldownEnded)
+            {
+                //todo: camera shake
+            }
+
+            if (_fallCooldown != null && _fallCooldown.CooldownEnded)
+            {
+                _fallCooldown = null;
+                StartCoroutine(Fall());
+            }
+
+            if (_restoreCooldown != null && _restoreCooldown.CooldownEnded)
+            {
+                _restoreCooldown = null;
+
+                rb.isKinematic = true;
+                rb.velocity = Vector2.zero;
+                platformCollider.isTrigger = false;
+                mesh.SetActive(true);
+                transform.position = _initialPosition;
+            }
+        }
+
+        private void OnCollisionEnter(Collision other)
+        {
+            Debug.Log("collision");
+            if (other.gameObject.layer.ToString().Equals("Player"))
+            {
+                if (other.contacts[0].normal.y >= -1 && other.contacts[0].normal.y < -0.5f)
+                {
+                    _fallCooldown = new Cooldown(timeBeforeFall);
+                    _fallCooldown.StartCooldown();
+                    _shakeCooldown = new Cooldown(timeBeforeFall / 2);
+                    _shakeCooldown.StartCooldown();
+                }
+            }
+        }
+
+        private IEnumerator Fall()
+        {
+            rb.isKinematic = false;
+            platformCollider.isTrigger = true;
+            yield return new WaitForSeconds(0.25f);
+
+            _shakeCooldown = null;
+            mesh.SetActive(false);
+            _restoreCooldown = new Cooldown(timeBeforeRespawn);
+            _restoreCooldown.StartCooldown();
+        }
+    }
+}
--- Assets/Scripts/Obstacles/Platforms/MovingPlatform.cs
+++ Assets/Scripts/Obstacles/Platforms/MovingPlatform.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using DG.Tweening;
 using UnityEngine;
@@ -11,21 +12,67 @@ namespace Obstacles.Platforms
         [SerializeField] private List<PlatformMovement> platformMovements;
         [SerializeField] private Transform platformTransform;
         [SerializeField] private float platformMovementsInterval = 1;
-        
-        [Header("Player detection")]
-        
+        [SerializeField] private PlayerInsideChecker checker;
         
         private Sequence _sequence;
-        private int _index = 0;
+        private int _index = 1;
+        private GameObject _player;
+        private bool _increment = true;
 
         private void Start()
         {
-            _sequence = DOTween.Sequence()
-                .Append(platformTransform.DOMove(platformMovements[_index].TargetPosition,
-                    platformMovements[_index].timeToAchieveTargetPos))
-                .AppendCallback(() => _index++)
-                .AppendCallback(() => _index = (_index >= platformMovements.Count ? 0 : _index))
-                .AppendInterval(platformMovementsInterval).SetLoops(-1);
+            MovePlatform();
+        }
+
+        private void Update()
+        {
+            CheckIfPlayerOn();
+
+            if (Vector3.Distance(platformMovements[_index].TargetPosition,
+                transform.position) < 0.2)
+            {
+                StartCoroutine(MoveCompleted());
+            }
+        }
+
+       private  IEnumerator  MoveCompleted()
+       {
+           if (_increment)
+               _index++;
+           else
+               _index--;
+
+           if (_index == platformMovements.Count - 1)
+               _increment = false;
+           else if (_index == 0)
+               _increment = true;
+           
+           yield return new WaitForSeconds(platformMovements[_index].timeToAchieveTargetPos);
+           MovePlatform();
+        }
+
+        private void CheckIfPlayerOn()
+        {
+            var player = checker.CheckIfPlayerInside();
+
+            if (_player == null && player != null)
+                _player = player;
+            
+            if(_player && player == null)
+                _player.transform.SetParent(null);
+            else if (player && player != null)
+                _player.transform.SetParent(transform);
+        }
+
+        private void MovePlatform()
+        {
+            platformTransform.DOMove(platformMovements[_index].TargetPosition,
+                platformMovements[_index].timeToAchieveTargetPos).SetEase(Ease.InOutSine);
+        }
+
+        private void OnDrawGizmos()
+        {
+            checker.OnDrawGizmos();
         }
     }
 }
